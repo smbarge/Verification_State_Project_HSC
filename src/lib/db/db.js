f85b9befccbed6d54e0cfe24bd44686d838fbaf8
User <smbarge@gamil.com>
2026-02-25 16:04:43 +0530

    change pg url

--- src/lib/db/db.js
+++ src/lib/db/db.js
@@ -3,20 +3,12 @@ const { Client } = pkg; // Destructure to get the Client class
 
 // Create a new client instance using the environment variable
 let connected = false;
-async function connectDb() {
-  try {
-    await client.connect();
-    console.log("Connected to PostgreSQL");
-    connected = true;
-  } catch (error) {
-    console.error("Connection error", error.stack);
-  }
-}
+
 
 export async function queryDb(query) {
   try {
     let DATABASE_URL =
-      "postgres://boarduser:admin@2025@3.110.205.109/hsc_board_verification_july25";
+      "postgres://verification_user:Admin@2026@13.201.171.187:5432/verification_database";
     const client = new Client({
       connectionString: DATABASE_URL,
     });
