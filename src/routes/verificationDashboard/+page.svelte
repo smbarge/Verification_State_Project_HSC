215365a19b6d4353d9c869960e811eb19617c50d
css04 <maheshwar@chanakyasoft.com>
2026-05-14 16:43:52 +0530

    change button name

--- src/routes/verificationDashboard/+page.svelte
+++ src/routes/verificationDashboard/+page.svelte
@@ -839,7 +839,7 @@
           type="button"
           class="w-full h-9 bg-blue-500 text-white text-sm rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
-          Recheck Support
+          Recheck Revert
         </button>
       </div>
     </form>
